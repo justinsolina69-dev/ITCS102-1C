1f61b1c2ff5bc56207a9e182657191ede185e760
justinsolina69-dev <justinsolina.69@gmail.com>
2025-08-17 21:35:10 +0800

    Create Code_Challenge1.py

---
+++ Code_Challenge1.py
@@ -0,0 +1,3 @@
+nameinput("Type your name :> --->")
+print("\t\t\t\t* \n \t\t\t* \t\t* \n \t\t* \t\t\t\t* ","name"," \t\t\t* \n \t* \t\t\t\t\t\t* \n \t\t* \t\t\t\t* \n \t\t\t* \t\t* \n \t\t\t\t* ")
+print(" Well done 👏🏽👏🏽 🎉 ")
