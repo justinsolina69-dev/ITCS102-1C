1cdda7595a9084320e9d2801bef939c022ea672a
justinsolina69-dev <justinsolina.69@gmail.com>
2025-08-19 10:22:46 +0800

    Update Code_Challenge2.py

--- Code_Challenge2.py
+++ Code_Challenge2.py
@@ -1,19 +1,30 @@
-Amountinput("Enter the Amount you want :> --->")
-print("1000:", "Amount //1000")
-Amount("1000%=  :> --->")
-print("500:", " Amount //500")
-Amount("500%=   :> --->")
-print("200:", " Amount //200")
-Amount("200%=   :> --->")
-print("100:", " Amount //100")
-Amount("100%=   :> --->")
-print("50:",  " Amount //50")
-Amount("50%=    :> --->")
-print("20:",  " Amount //20")
-Amount("20%=    :> --->")
-print("10:",  " Amount //10")
-Amount("10%=    :> --->")
-print("5:",   " Amount //5")
-Amount("5%=     :> ---> ")
-print("1:",   " Amount //1")
-Amount("1%=     :> ---> ")
+amounteval(input(" Enter you amount deposit :> "))
+print("Here is the breakdown of your deposit")
+"Thousand + amount //1000"
+"Thousand_change = amount - thousand * 1000"
+print( "Thousand , _ 1000")
+print("Thousand_change")
+"Thousand_change = amount - thousand * 500"
+print( "Thousand , _ 500")
+print("Thousand_change")
+"Thousand_change = amount - thousand * 200"
+print( "Thousand , _ 200")
+print("Thousand_change")
+"Thousand_change = amount - thousand * 100"
+print( "Thousand , _ 100")
+print("Thousand_change")
+"Thousand_change = amount - thousand * 50"
+print( "Thousand , _ 50")
+print("Thousand_change")
+"Thousand_change = amount - thousand * 20"
+print( "Thousand , _ 20")
+print("Thousand_change")
+"Thousand_change = amount - thousand * 10"
+print( "Thousand , _ 10")
+print("Thousand_change")
+"Thousand_change = amount - thousand * 5"
+print( "Thousand , _ 5")
+print("Thousand_change")
+"(Thousand_change = amount - thousand * 1")
+print( "Thousand , _ 1")
+print("Thousand_change")
