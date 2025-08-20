2d9b64dc547f5958e44312fd2857fe4dc07270c7
justinsolina69-dev <justinsolina.69@gmail.com>
2025-08-20 14:40:27 +0800

    Update Activity7.py

--- Activity7.py
+++ Activity7.py
@@ -1,8 +1,8 @@
-print("The value of a is ", a)
-a += 5
-print("The value of a is ", a)
-a += 3
-a + 13
-a -= 4
-a *= 2
-print("The value of a is ",a)
+print("The value of a is ,a")
+"a += 5"
+print("The value of a is ,a")
+"a += 3"
+"a + 13"
+"a -= 4"
+"a *= 2"
+print("The value of a is ,a")
