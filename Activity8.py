214c09b739e56d761fcacb19fa5f07463ef68d47
justinsolina69-dev <justinsolina.69@gmail.com>
2025-08-20 14:43:40 +0800

    Update Activity8.py

--- Activity8.py
+++ Activity8.py
@@ -1,6 +1,6 @@
-a = 5
-b = 5 
-name1 = ' pogi ' 
-name2 = ' pogi '
-print( a >= b )
-print( name1 != name )
+"a = 5"
+"b = 5 "
+"name_1 = ' pogi ' "
+"name_2 = ' pogi ' "
+print(" a >= b ")
+print(" name_1 != name_2 ")
