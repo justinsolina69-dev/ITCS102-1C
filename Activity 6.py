22c4e482d9cda331ccbeea1bbde380856210c173
Sölî 6699 <justinsolina.69@gmail.com>
2025-08-21 21:04:04 +0800

    Update Activity 6.py

--- Activity 6.py
+++ Activity 6.py
@@ -1,5 +1,5 @@
 First_numeval(input("Input a number :> -->"))
 Second_numeval(input("Input another number :> -->"))
 answer(First_num + Second_num)
-print("The answer is",answer(
+print("The answer is",answer)
   
