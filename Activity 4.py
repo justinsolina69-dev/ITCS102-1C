369340b867801241cdbdd94143e74f72f2a1cef5
Sölî 6699 <justinsolina.69@gmail.com>
2025-08-21 21:25:23 +0800

    Update Activity 4.py

--- Activity 4.py
+++ Activity 4.py
@@ -1,3 +1,3 @@
 nameinput("Enter your name player --->")
-print ("Wellcome to the Matrix! ","name")
-print ("Your name has ", len("name"),"Chapter")
+print ("Wellcome to the Matrix! ")(name(
+print ("Your name has ", len(name(),"Chapter")
