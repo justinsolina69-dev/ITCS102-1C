138917908010313dbdf501c84e6e2a978a525e60
Sölî 6699 <justinsolina.69@gmail.com>
2025-08-23 06:44:39 +0800

    Update Activity8.py

--- Activity8.py
+++ Activity8.py
@@ -1,6 +1,6 @@
-a( = 5
-b( = 5 
-name_1(=  pogi(
-name_2( =  pogi(
-print( a( >= b( ),
-print( name_1( != name_2()
+a = 5
+b = 5 
+(name_1( =  pogi()
+(name_2( =  pogi(),
+print( (a( >= b( ),
+print( (name_1( != name_2()
