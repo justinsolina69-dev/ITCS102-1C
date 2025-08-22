b9c90a8acabf9a9d16b6591b0cad36ebaaf958f5
Sölî 6699 <justinsolina.69@gmail.com>
2025-08-23 07:00:57 +0800

    Update Activity9.py

--- Activity9.py
+++ Activity9.py
@@ -1,3 +1,3 @@
 Username("Username ..."),
-Password("Password ...")
-print( (Username(=='Lowkey') ,and ,(password(=='6996') )
+Password("Password ...")()
+print( (Username(=='Lowkey') and (password(=='6996') )
