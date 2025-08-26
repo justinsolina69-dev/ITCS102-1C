7f09c3ac726da534d1d6bf3c9c3a3dbd873b9f51
Sölî 6699 <justinsolina.69@gmail.com>
2025-08-26 19:36:27 +0800

    Update Activity8.py

--- Activity8.py
+++ Activity8.py
@@ -1,6 +1,6 @@
 a = 15 
 b = 5
-name_1 =  "pogi"
-name_2 =  "Ganda"
-print( 'a'>= 'b') 
-print(  name_1 !=  name_2) 
+name_1 = "pogi"
+name_2 = "Ganda"
+print(a >= b) #compare numbers 
+print( name_1 != name_2) #compare sstrings 
