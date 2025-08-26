cd98359552a720bc337c6c5a927a3de71ec2b5c4
Sölî 6699 <justinsolina.69@gmail.com>
2025-08-26 19:19:30 +0800

    Update Activity8.py

--- Activity8.py
+++ Activity8.py
@@ -1,6 +1,8 @@
-a=15 
-a=5
-name_1( =  "pogi"
-name_2( =  "Ganda"
-print( a >= b) #compare numbers 
-print(  name_1 !=  name_2) #compare strings
+a = 15 
+a = 5
+name_1 =  "pogi"
+name_2 =  "Ganda"
+print( a >= b) 
+#compare numbers 
+print(  name_1 !=  name_2) 
+#compare strings
