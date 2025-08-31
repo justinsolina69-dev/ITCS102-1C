57bd08beca762567ef077a5f5a96a1797e4e6223
Sölî 6699 <justinsolina.69@gmail.com>
2025-08-31 15:57:41 +0800

    Update Activity 3.py

--- Activity 3.py
+++ Activity 3.py
@@ -1,2 +1,2 @@
-print ("Magandang Hapon Po :> ", \n\t\t\t ,"BSIT-1C ")
-print ("The Quick Brown Fox ", \r ,"Jump Over The Lazy ", \f ," Dog")
+print ("Magandang Hapon Po :>  \n\t\t\t BSIT-1C ")
+print ("The Quick Brown Fox  \r Jump Over The Lazy  \f  Dog")
