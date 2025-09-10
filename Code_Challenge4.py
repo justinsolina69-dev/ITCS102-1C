dc55b295cdaa01ddb983c93d438ffe11af787ffe
Sölî 6699 <justinsolina.69@gmail.com>
2025-09-10 21:53:14 +0800

    Update and rename Code_Challenge3.py to Code_Challenge4.py

---
+++ Code_Challenge4.py
@@ -0,0 +1,42 @@
+name = input (" hi my fellow weeb what's your name : ")
+print(" Hello  ",name," Welcome to my Mangga recommendation   ")
+print("please select your favorite genre: ")
+
+print("fantacy ")
+print(" romance ")
+print("  horror  ")
+genre_choice = input("enter your choice (fantacy/romace/horror):")
+print(" select what year: ")
+print("2000")
+print("2005")
+print("2010")
+m = input(" select  year year (2000/2005/2010):" )
+
+
+if genre_choice == "Fantacy":
+    choice = genre_choice 
+    print(" \n you selected:", choice)     
+
+if genre_choice == "romance":
+    choice = genre_choce
+    print(" \n you selected:" , choice) 
+      
+if genre_choice  == "horror":
+    choice = genre_choice''
+    print(" \n you selected:", choice) 
+         
+if m == 2000:
+    2000 = "demonSlayer"
+    print(" \n you selected:", 2000 )    
+
+if m == 2005:
+    2005 = "reincarnation as a slime"
+    print(" \n you selected:", 2005 )     
+              
+if  m == 2010:
+    2010 = "berserk"
+    print(" \n you selected:", 2010  )
+  
+
+
+
