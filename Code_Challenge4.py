0bd1eb4ca3851ef9c3b19629e379a63a8da08f46
Sölî 6699 <justinsolina.69@gmail.com>
2025-09-11 11:29:20 +0800

    Update Code_Challenge4.py

--- Code_Challenge4.py
+++ Code_Challenge4.py
@@ -1,42 +1,49 @@
-name = input (" hi my fellow weeb what's your name : ")
-print(" Hello  ",name," Welcome to my Mangga recommendation   ")
-print("please select your favorite genre: ")
+name = input("Hi whats your name ")
+print("Hello " ,name ,"Welcome to my Mangga Recommendation")
+Genre = input("Select any genre you want\n (Fantasy, Romance , Horror) : ")
+Size = input("Select any \n (Short/Medium/Long) : ")
+Year = input("Select year you want \n (2000/2005/2010) : ")
 
-print("fantacy ")
-print(" romance ")
-print("  horror  ")
-genre_choice = input("enter your choice (fantacy/romace/horror):")
-print(" select what year: ")
-print("2000")
-print("2005")
-print("2010")
-m = input(" select  year year (2000/2005/2010):" )
-
-
-if genre_choice == "Fantacy":
-    choice = genre_choice 
-    print(" \n you selected:", choice)     
-
-if genre_choice == "romance":
-    choice = genre_choce
-    print(" \n you selected:" , choice) 
-      
-if genre_choice  == "horror":
-    choice = genre_choice''
-    print(" \n you selected:", choice) 
-         
-if m == 2000:
-    2000 = "demonSlayer"
-    print(" \n you selected:", 2000 )    
-
-if m == 2005:
-    2005 = "reincarnation as a slime"
-    print(" \n you selected:", 2005 )     
-              
-if  m == 2010:
-    2010 = "berserk"
-    print(" \n you selected:", 2010  )
+#1
+if Genre.lower() == "Fantasy":
+    Answer = "Genre"
+    print(" \n You selected ", Answer)
+    
+if Size.lower() == "Short":
+    Answer = "Size"
+    print(" \n You selected ", Answer)
   
+if Year == "2000":
+    Answer = "Naruto"
+    print(" \n You selected " , Answer)
+#2
+if Genre.lower() == "Romance":
+    Answer = "Genre"
+    print(" \n You selected", answer)
+    
+if Size.lower() == "medium":
+    Answer = "Size"
+    print(" \n You selected ", Answer)
+  
+if Year == "2005":
+    Answer = "lovestrike"
+    print(" \n You selected " , Answer)    
+#3
+if Genre.lower() == "Horror":
+    Answer = "Genre"
+    print(" \n You selected ", Answer)
+    
+if Size.lower() == "Short":
+    Answer = "Size"
+    print(" \n You selected ", Answer)
+  
+if Year == "2000":
+    Answer = "Naruto"
+    print(" \n You selected " , Answer)
+
+
+
+
 
 
 
