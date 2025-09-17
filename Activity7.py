ae4656861fb1b596cf92bf9b4871441bcccb3dfd
Sölî 6699 <justinsolina.69@gmail.com>
2025-09-17 10:22:32 +0800

    Update Activity7.py

--- Activity7.py
+++ Activity7.py
@@ -1,8 +1,15 @@
-print("The value of a is "),a(),
-a(+=5)
-print("The value of a is "),a(),
-a(+=5)
-a(+=13)
-a(-=14)
-a(*=2)
-print("The value of a is",a()
+a = eval(input("Enter any number: "))
+a += 5
+print("The value of a is" , (a))
+a = eval(input("Enter any number :"))
+a -=5
+print("The value of a is" , (a))
+a = eval(input("Enter any number: "))
+a *= 5
+print("The value of a is" , (a))
+
+
+
+
+
+
