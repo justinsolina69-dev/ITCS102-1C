12eacc05b89c3a1241b6e9dce4c74d6e60b5bce4
Sölî 6699 <justinsolina.69@gmail.com>
2025-09-17 09:24:03 +0800

    Update Activity 2.py

--- Activity 2.py
+++ Activity 2.py
@@ -1,2 +1,4 @@
 name = input("What is your name?")
-print("Hi!"name," Welcome to the Matrix!")
+print("Hi!",name," Welcome to the Matrix!")
+Age = input("How old are you ?:")
+print("Wow",age,"Your so cool!")
