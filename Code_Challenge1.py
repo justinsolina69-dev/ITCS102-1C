422252b5825e941dfaecf9c326268651fbb861ab
Sölî 6699 <justinsolina.69@gmail.com>
2025-09-17 12:03:27 +0800

    Update Code_Challenge1.py

--- Code_Challenge1.py
+++ Code_Challenge1.py
@@ -1,3 +1,4 @@
-nameinput("Type your name :> --->")
-print("\t\t\t\t* \n \t\t\t* \t\t* \n \t\t* \t\t\t\t* ","name"," \t\t\t* \n \t* \t\t\t\t\t\t* \n \t\t* \t\t\t\t* \n \t\t\t* \t\t* \n \t\t\t\t* ")
+name = input("Type your name : ")
+print("\t\t\t\t* \n \t\t\t* hi \t\t* \n \t\t* \t\t\t\t* \n  \t\t\t",name," \n \t* \t\t\t\t\t\t* \n \t\t* \t\t\t\t* \n \t\t\t* \t\t* \n \t\t\t\t* ")
 print(" Well Done! ")
+
