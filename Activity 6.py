315cdfc5d40c3fc2181fe9d39cfd6290b26e7515
Sölî 6699 <justinsolina.69@gmail.com>
2025-09-17 09:36:16 +0800

    Update Activity 6.py

--- Activity 6.py
+++ Activity 6.py
@@ -1,4 +1,4 @@
 First_num = eval(input("Input a number:"))
 Second_num = eval(input("Input another number : "))
-First_num() + (Second_num() == answer()
+First_num() + (Second_num() = answer()
 print("The answer is",answer())
