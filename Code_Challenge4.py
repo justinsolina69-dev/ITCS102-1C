fc74dfad55e4f0ca6cfb322bd422b688177cb5c6
Sölî 6699 <justinsolina.69@gmail.com>
2025-09-17 15:34:55 +0800

    Update Code_Challenge4.py

--- Code_Challenge4.py
+++ Code_Challenge4.py
@@ -1,49 +1,44 @@
 name = input("Hi whats your name ")
 print("Hello " ,name ,"Welcome to my Mangga Recommendation")
-Genre = input("Select any genre you want\n (Fantasy, Romance , Horror) : ")
-Size = input("Select any \n (Short/Medium/Long) : ")
+Genre = input("Select any genre you want \n (Fantasy, Romance , Horror) : ")
+Size = input("Select any Size \n (Short/Medium/Long) : ")
 Year = input("Select year you want \n (2000/2005/2010) : ")
 
 #1
 if Genre.lower() == "Fantasy":
-    Answer = "Genre"
-    print(" \n You selected ", Answer)
+    print(" \n You selected: ", Genre)
+    if Year == "2000":
+        print("  I recommend: \n Moonlight ")
+    elif Year == "2005":
+        print(" I recommend: \n Berserk ")
+    elif Year == "2010":
+        print(" I recommend: \n Zom100 ")
+    else:
+        print("  \n Sorry we dont have Fantasy mangga Available ")
     
-if Size.lower() == "Short":
-    Answer = "Size"
-    print(" \n You selected ", Answer)
-  
-if Year == "2000":
-    Answer = "Naruto"
-    print(" \n You selected " , Answer)
-#2
+#2 
 if Genre.lower() == "Romance":
-    Answer = "Genre"
-    print(" \n You selected", Answer)
+    print(" \n You selected: ", Genre)
+    if Year == "2000":
+        print(" I recommend: \n AttackOnTitan ")
+    elif Year == "2005":
+        print(" \n I recommend: \n  Cleavates ")
+    elif Year == "2010":
+        print(" \n I recommend: \n Darkness ")
+    else:
+        print("  \n Sorry we dont have Romance mangga Available ")
     
-if Size.lower() == "medium":
-    Answer = "Size"
-    print(" \n You selected ", Answer)
-  
-if Year == "2005":
-    Answer = "lovestrike"
-    print(" \n You selected " , Answer)    
 #3
 if Genre.lower() == "Horror":
-    Answer = "Genre"
-    print(" \n You selected ", Answer)
-    
-if Size.lower() == "Short":
-    Answer = "Size"
-    print(" \n You selected ", Answer)
-  
-if Year == "2000":
-    Answer = "Naruto"
-    print(" \n You selected " , Answer)
-
-
-
-
-
-
+    print("You selected: ", Genre)
+    if Year == "2000":
+        print("I recommend:  \n Highland"  )
+    elif Year == "2005":
+        print(" I recommend:  \n Promise never land" )
+    elif Year == "2010":
+        print(" I recommend:  \n Doomsday ")
+        else:
+            print("  \n Sorry we dont have Horror mangga  Available ")
+else:
+print("Sorry we dont have Available mangga ")
 
