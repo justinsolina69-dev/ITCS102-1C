1a573349cb8166140fabe592ff6fea65473dcd32
Sölî 6699 <justinsolina.69@gmail.com>
2025-09-17 09:34:10 +0800

    Update Activity 5.py

--- Activity 5.py
+++ Activity 5.py
@@ -1,5 +1,3 @@
-name = eval(input("Type anything :"))
-print("The data type name is"),type(name))
-                                    
+                                  
 name = eval(input("Hi type anything you want:")
 print("\n the data type name is ", type(name))
