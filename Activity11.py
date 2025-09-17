2ccfac150c944aaad11512d6e932c4bdc18efb06
Sölî 6699 <justinsolina.69@gmail.com>
2025-09-17 11:09:31 +0800

    Update Activity11.py

--- Activity11.py
+++ Activity11.py
@@ -1,9 +1,11 @@
 temp = eval(input("enter temperature :"))
 
-if temp >= 0:
-    print("cold whoooooo")
-elif temp >= 2 and temp <= 5: print("super cold   achoooooo!")
-elif temp >= 5 and temp <= 10:
-    print("extreemly cold  ahhhhhhhhhhh! ")
+if temp <= 5:
+    print("Mild Cold ")
+elif temp >= 5  and temp <= 10: print("Super Cold   Achoooooo!")
+elif temp >= 10 and temp <= 15:
+    print("Extremely Cold  Ahhhhhhhhhhh! ")
+elif temp >= 15 and temp <=20:
+    print("DANGER!!!")
 else:
     print("invalid temperature")
