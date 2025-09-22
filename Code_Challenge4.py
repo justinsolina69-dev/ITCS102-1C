e19b9a8600fb59259fb47cf7a5a6a86dc0e0215b
LOWKEY <justinsolina.69@gmail.com>
2025-09-22 14:21:21 +0800

    Update Code_Challenge4.py

--- Code_Challenge4.py
+++ Code_Challenge4.py
@@ -1,44 +1,35 @@
-name = input("Hi whats your name ")
-print("Hello " ,name ,"Welcome to my Mangga Recommendation")
-Genre = input("Select any genre you want \n (Fantasy, Romance , Horror) : ")
-Size = input("Select any Size \n (Short/Medium/Long) : ")
-Year = input("Select year you want \n (2000/2005/2010) : ")
+print("Welcome to my Mangga Recommendation")
+print("Choode wisely")
 
-#1
-if Genre.lower() == "Fantasy":
-    if Size.lower() == "Short":
-        print(" \n You selected: ", Size )
-        if Year == "2000":
-            print("  I recommend: \n Moonlight ")
-            if Year == "2005":
-                print(" I recommend: \n Berserk ")
-                if Year == "2010":
-                    print(" I recommend: \n Zom100 ")
-                 
-               
-    
-#2 
-if Genre.lower() == "Romance":
-     if Size.lower() == "medium":
-         print(" \n You selected: ", Genre)
-         if Year == "2000":
-             print(" I recommend: \n AttackOnTitan ")
-             if Year == "2005":
-                 print(" \n I recommend: \n  Cleavates")
-                 if Year == "2010":
-                     print(" \n I recommend: \n Darkness ")
-              
-    
-#3
-if Genre.lower() == "Horror":
-     if Size.lower() == "long":
-         print("You selected: ", Genre)
-         if Year == "2000":
-             print("I recommend:  \n Highland"  )
-             if Year == "2005":
-                 print(" I recommend:  \n Promise never land")
-                 if Year == "2010":
-                     print(" I recommend:  \n Doomsday ")
+genre = input("Select any genre you want\n (fantasy ,romance,horror) : ").lower()
+year = input("Select year you want\n (2000s ,2005s,2010s) : ").lower()
+
+if genre == "fantasy":
+        if year == "2000s":
+            print("I recommend: Moonlight ")
+        elif year == "2005s":
+            print(" I recommend: Berserk")
+        elif year == "2010s":
+            print(" I recommend: Zom100")
+        else:
+            print(" no fantasy  recommendation available ")                     
+elif genre == "romance":
+    if year == "2000s":
+        print(" I recommend: AOT")
+    elif year == "2005s":
+        print(" I recommend: Cleavates")
+    elif year == "2010s":
+        print(" I recommend: Darkness")
+    else:
+        print("No romance recommendation available")         
+elif genre == "horror":
+    if year == "2000s":
+        print("I recommend: Highland")
+    elif year == "2005s":
+        print(" I recommend:  Promiseneverland")
+    elif year == "2010s":
+        print(" I recommend: Doomsday")
+    else:
+           print("No horror available")                  
 else:
-    print("Sorry we dont have Available mangga ")
-
+    print("No mangga available")
