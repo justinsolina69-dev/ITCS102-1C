5deffaf56d4763b0be251d090c7fd132aafd1cc3
LOWKEY <justinsolina.69@gmail.com>
2025-10-08 11:56:42 +0800

    Update Activity19.py

--- Activity19.py
+++ Activity19.py
@@ -1,1 +1,17 @@
+for J in range (1,11,1):
+  for S in range(10,1,-1):
+    print("", end = "")
+  for O in range(1,1,1):
+    print("*", end = "")
+  for A in range(1,1,1):
+    print("*", end = "")
+print()
 
+for J in range(1,11,1):
+  for S in range(1,1,1):
+    print("", end = "")
+  for O in range(10,1,-1):
+    print("*", end = "")
+  for A in range(10,J,-1):
+    print("*", end = "")
+print()
