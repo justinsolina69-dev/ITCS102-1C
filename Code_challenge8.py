071019128972d337894b34a0c0bd2eddbc5d67b5
LOWKEY <justinsolina.69@gmail.com>
2025-10-11 00:45:08 +0800

    Update Code_challenge8.py

--- Code_challenge8.py
+++ Code_challenge8.py
@@ -8,3 +8,25 @@ print("Here's the result:")
 for S in range(1,11,):
  print(numero ,  "x", S ,  "=", numero * S)
 
+print("MULTIPLICATION TABLE MAKER")
+number = eval(input("Enter a number: "))
+sum = 0
+print("Here's the result:")
+for J in range(1,11,1):
+    print(number,  "x", J,  "=",number * J)
+print("\n")
+for S in range(11,21,1):
+     print(number,"x" , S, "=",number * S)
+print("\n")
+for O in range(21,31,1):
+     print(number ,  "x", O,  "=", number * O)
+print("\n")
+for A in range(31,41,1):
+     print(number,"x" , A, "=" , number * A)
+print("\n")
+for B in range(41,51,1):
+     print(number ,  "x", B,  "=", number * B)
+print("\n")                  
+
+    
+    
