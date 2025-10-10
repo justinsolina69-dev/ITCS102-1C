d4fbb4e82ec4e4111be9422811880689a371d042
LOWKEY <justinsolina.69@gmail.com>
2025-10-10 18:01:49 +0800

    Update Code_challenge12.py

--- Code_challenge12.py
+++ Code_challenge12.py
@@ -1,1 +1,27 @@
+for J in range(1,6,1):
+  for S in range(11,J,-1):
+    print(" ",end=" ")
+  for O in range(J,0,-1):
+    print(O,end=" ")
+  for A in range(2,J+1,1):
+    print(A,end=" ")
+  print()
+
+for J in range(1,10,1):
+  for S in range(11,J,-1):
+    print(" ",end=" ")
+  for O in range(J,0,-1):
+    print(O,end=" ")
+  for A in range(2,J+1,1):
+    print(A,end=" ")
+  print()
+for J in range(9,0,-1):
+  for S in range(11,J,-1):
+    print(" ",end=" ")
+  for O in range(J,0,-1):
+    print(O,end=" ")
+  for A in range(2, J+1,1):
+    print(A,end=" ")
+  print()
+
 
