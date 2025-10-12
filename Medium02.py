01509599c36433bad2e93b1fb150f8d5d8249f3a
LOWKEY <justinsolina.69@gmail.com>
2025-10-12 17:20:19 +0800

    Update Medium02.py

--- Medium02.py
+++ Medium02.py
@@ -1,1 +1,22 @@
+print("Medium2.py")
+print("MULTIPLICATION TABLE MAKER")
+number = eval(input("Enter a number: "))
+sum = 0
+
+print("Here's the result:")
+for J in range(1,11,1):
+    print(number,  "x", J,  "=",number * J)
+print(" \t")
+for S in range(11,21,1):
+     print(number,"x" , S, "=",number * S)
+print("\t")
+for O in range(21,31,1):
+     print(number ,  "x", O,  "=", number * O)
+print("\t")
+for A in range(31,41,1):
+     print(number,"x" , A, "=" , number * A)
+print("\t")
+for B in range(41,51,1):
+     print(number ,  "x", B,  "=", number * B)
+print("\t")                  
 
