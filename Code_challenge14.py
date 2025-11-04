ab64a3bad3de503da2958f5b52ac4cf462609c9c
LOWKEY <justinsolina.69@gmail.com>
2025-11-04 18:38:06 +0800

    Improve anime list display and exit handling
    Refactor anime entry program to improve output display.

--- Code_challenge14.py
+++ Code_challenge14.py
@@ -8,25 +8,15 @@ Hanime_list = []
 I_love_you = True
 
 while I_love_you == True:
-
     Hanime_Title= input("Enter any Title.(and type \"exit\" to stop): ").lower()
 
-
     if Hanime_Title.lower() == "exit":
-
         print("You have exited the anime entry program")
-
         I_love_you = False
-
-        
-
     else:
-
         Hanime_list.append(Hanime_Title)
+        print("Your anime list includes: ")
+    for Hanime_Title in Hanime_list:
+        print(f"-->{Hanime_Title}")
 
-print("Your anime list includes: ")
-
-for Hanime_Title in Hanime_list:
-
-    print(f"-->{Hanime_Title}")
 
